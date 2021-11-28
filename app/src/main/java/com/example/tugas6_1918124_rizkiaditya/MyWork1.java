d641e57c7fa4b358d27857e2751b0a77e7ad4856
rizkiaditya7 <rizkiaditya20@gmail.com>
2021-11-28 17:41:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tugas6_1918124_rizkiaditya/MyWork1.java
@@ -0,0 +1,43 @@
+package com.example.tugas6_1918124_rizkiaditya;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWork1 extends Worker {
+
+    public MyWork1(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        displayNotification("Ini adalah Task Anda", "Anda Memilih SepakBola");
+        return Result.success();
+    }
+
+    private void displayNotification(String task, String desc) {
+
+        NotificationManager manager =(NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationChannel channel = new NotificationChannel("mw2","test123", NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),
+                "mw2")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+
+
+        manager.notify(1, builder.build());
+    }
+}
